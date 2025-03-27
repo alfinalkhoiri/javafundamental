82648fdbad8543315945e41042ffb2502ce76a61
alfinalkhoiri_ <alfinalkhoiri@gmail.com>
2025-03-27 19:29:55 +0700

    First Commit

---
+++ src/main/java/com/juaracoding/TestTemplate.java
@@ -0,0 +1,15 @@
+package com.juaracoding;
+
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author alfin a.k.a. Alfin Al Khoiri
+Java Developer
+Created on 27/03/2025 19.28
+@Last Modified 27/03/2025 19.28
+Version 1.0
+*/
+
+public class TestTemplate {
+}
